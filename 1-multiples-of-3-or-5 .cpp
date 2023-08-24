cc8b911f3076f66254112fdb10bb8d5805570d4e
AbhinnFromIndia <vermaabhinn@gmail.com>
2023-08-25 00:34:19 +0530

    Finish solution to 4-... and syntax formatting for rest

--- 1-multiples-of-3-or-5 .cpp
+++ 1-multiples-of-3-or-5 .cpp
@@ -1,17 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    /*OBJECTIVE - Find the sum of all the multiples of 3 or 5 below 1000*/    
+int main()
+{
+    /*OBJECTIVE - Find the su
+    m of all the multiples of 3 or 5 below 1000*/
     int LIMIT = 1000;
     int sum = 0;
 
-    for (int i=0; i<LIMIT; i++) {
-        if (i % 3 == 0 || i % 5 == 0) 
-            sum += i;    
+    for (int i = 0; i < LIMIT; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
     }
 
-    cout << "Sum of all multiples of 3 or 5: "<< sum;
-
-    return 1;
+    cout << "Sum of all multiples of 3 or 5: " << sum;
 }
